196288bf792659c972ed2ee22e1e5d3319f888c4
Moe-hacker <admin@crack.moe>
2023-11-24 11:40:24 +0800

    site update Fri Nov 24 11:40:24 AM CST 2023

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BK_REPO=../site-backup
+BLOG_DIR=$(pwd)
+cp -rv ./public ${BK_REPO}/
+cp -rv ./source ${BK_REPO}/
+cp -v ./_config.yml ${BK_REPO}/
+cp -v ./node_modules/hexo-theme-butterfly/_config.yml  ${BK_REPO}/_config-butterfly.yml
+cp -v ./package*.json ${BK_REPO}/
+cp -rv ./push.sh ${BK_REPO}/
+cd ${BK_REPO}
+git add .
+git commit -m "site update $(date)"
+git push
+cd ${BLOG_DIR}
+./node_modules/hexo/bin/hexo g
+./node_modules/hexo/bin/hexo d
